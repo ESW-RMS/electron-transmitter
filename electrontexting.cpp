22656379aadb58e641174c354159d7040066801e
riyav <riyav@stanford.edu>
2016-08-15 15:41:50 +0700

    Delete electrontexting.cpp

--- electrontexting.cpp
+++
@@ -1,9 +0,0 @@
-void sendSMS_manual(String message, String phoneNumber){
-//probably want delays for this
-  String setPhone = "AT+CMGS=";
-  setPhone+=phoneNumber;
-  int numberSet = Cellular.command(setPhone);
-  int messageSent = Cellular.command(message);
-  int messageEnded = Cellular.command(String((char)26).c_str()); //the ASCII code of the ctrl+z is 26)
-}
-
